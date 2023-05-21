d2ad18f223effa05b6db0bbd095c924098675682
Мария Таскина <maryyysh2016@mail.ru>
2023-05-22 02:02:43 +0300

    step 5 rev.3

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -19,24 +19,37 @@ public class Differ {
         Path getAbsFirstPath = getFirstPath.toAbsolutePath().normalize();
         // Читаем файл
         String file1 = Files.readString(getAbsFirstPath); //переводим строку в данные (файл)
-        ObjectMapper objectMapperFile1 = new ObjectMapper(); //создаем объект класса ObjectMapper
+        // Создайте объект ObjectMapper, который является основным классом для обработки JSON-данных в Jackson:
+        ObjectMapper objectMapperFile1 = new ObjectMapper();
+        // Используйте метод readValue() для чтения JSON-файла и преобразования его в объект Java:
         // Метод readValue() используется для преобразования (десериализации) JSON из строки, потока или файла в POJO.
-        // Usage is by sub-classing
+
+        //Мы можем преобразовать JSON в Java Map, что очень удобно, если мы не знаем,
+        //чего ожидать от файла JSON, который мы пытаемся спарсить.
+        // ObjectMapper превратит имя каждой переменной в JSON в ключ для Map,
+        // а значение этой переменной — в значение по этому ключу.
         Map<String, Object> map1 = objectMapperFile1.readValue(file1, new TypeReference<Map<String,Object>>(){}); //JSON to Java Object
 
         Path getSecondPath = Paths.get(filepath2);
         Path getAbsSecondPath = getSecondPath.toAbsolutePath().normalize();
         String file2 = Files.readString(getAbsSecondPath);
         ObjectMapper objectMapperFile2 = new ObjectMapper();
-        Map<String, Object> map2 = objectMapperFile2.readValue(file2, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> map2 = objectMapperFile2.readValue(file2, new TypeReference<>(){});
 
         Set<String> keyFile = new TreeSet<>(map1.keySet());
         keyFile.addAll(map2.keySet());
 
         String result = "{\n";
         for(String key: keyFile) {
-            if (map1.containsKey(key) && map2.containsKey(key)) && Objects.equals(map1.get(key), map2.get(key)){
-                result += String.format("");
+            // без изменений
+            if (map1.containsKey(key) && map2.containsKey(key) && map1.get(key).equals(map2.get(key))) {
+                result += String.format("    %s: %s\n", key, map1.get(key));
+            } else if () {
+//                result += String.format("    %s: %s\n", key, );
+            } else if () {
+//                result += String.format("    %s: %s\n", key, );
+            } else () {
+//                result += String.format("    %s: %s\n", key, );
             }
         }
     }
