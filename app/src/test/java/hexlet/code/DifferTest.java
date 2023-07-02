50953048172d6f1261d30fea082d92e2eb25b5ab
Мария Таскина <maryyysh2016@mail.ru>
2023-07-03 00:34:57 +0300

    step 7 rev. 3 fix tests Java CI

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DifferTest {
-    private final String DEFAULT_FORMAT = "stylish";
+    private static String defaultFormat = "stylish";
     private static String jsonFile1;
     private static String jsonFile2;
     private static String jsonParsingResult;
@@ -25,19 +25,18 @@ public class DifferTest {
         yamlFile2 = "./src/test/resources/file2.json";
         yamlParsingResult = Files.readString(Paths.get("./src/test/resources/yamlParsingResult"));
     }
-/*    @Test
+    @Test
     public void testDefaultFormat() throws Exception {
-        String jsonResult = Differ.generate(jsonFile1, jsonFile2); // написать перегрузку метода
+        String jsonResult = Differ.generate(jsonFile1, jsonFile2);
         assertThat(jsonResult).isEqualTo(jsonParsingResult);
-        String yamlResult = Differ.generate(yamlFile1, yamlFile2); // написать перегрузку метода
+        String yamlResult = Differ.generate(yamlFile1, yamlFile2);
         assertThat(yamlResult).isEqualTo(yamlParsingResult);
     }
- */
     @Test
     public void testStylishFormat() throws Exception {
-        String jsonResult = Differ.generate(jsonFile1, jsonFile2, DEFAULT_FORMAT);
+        String jsonResult = Differ.generate(jsonFile1, jsonFile2, defaultFormat);
         assertThat(jsonResult).isEqualTo(jsonParsingResult);
-        String yamlResult = Differ.generate(yamlFile1, yamlFile2, DEFAULT_FORMAT);
+        String yamlResult = Differ.generate(yamlFile1, yamlFile2, defaultFormat);
         assertThat(yamlResult).isEqualTo(yamlParsingResult);
     }
 }
