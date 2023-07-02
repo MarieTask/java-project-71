ded940f29b4f6f9369ca9e8d78c1d40f1e23da7d
Мария Таскина <maryyysh2016@mail.ru>
2023-07-03 00:31:29 +0300

    step 7 rev. 2 fix tests

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DifferTest {
-    private static String DEFAULT_FORMAT = "stylish";
+    private final String DEFAULT_FORMAT = "stylish";
     private static String jsonFile1;
     private static String jsonFile2;
     private static String jsonParsingResult;
