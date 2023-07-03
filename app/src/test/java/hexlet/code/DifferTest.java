fc642fbf131508c03de9bf3e5941877dbcab7ee7
Мария Таскина <maryyysh2016@mail.ru>
2023-07-04 01:58:44 +0300

    step 8

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -19,8 +19,8 @@ public class DifferTest {
     public static void beforeAll() throws Exception {
         jsonFile1 = "./src/test/resources/file1.json";
         jsonFile2 = "./src/test/resources/file2.json";
-        yamlFile1 = "./src/test/resources/file1.json";
-        yamlFile2 = "./src/test/resources/file2.json";
+        yamlFile1 = "./src/test/resources/file1.yml";
+        yamlFile2 = "./src/test/resources/file2.yml";
         stylishParsingResult = Files.readString(Paths.get("./src/test/resources/stylishParsingResult"));
     }
     @Test
