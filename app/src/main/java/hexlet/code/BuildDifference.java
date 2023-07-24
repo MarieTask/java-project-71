efd2f7b00fd9b208e0fc0edbc5f508be19ad2af7
Мария Таскина <maryyysh2016@mail.ru>
2023-07-24 21:38:47 +0300

    fix errors rev.2

--- app/src/main/java/hexlet/code/BuildDifference.java
+++ app/src/main/java/hexlet/code/BuildDifference.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 
 
 public class BuildDifference {
-    public static List<Map<String,Object>> buildDifference(Map<String, Object> map1, Map<String, Object> map2) {
+    public static List<Map<String, Object>> buildDifference(Map<String, Object> map1, Map<String, Object> map2) {
         List<Map<String, Object>> result = new ArrayList<>();
 
         Set<String> unionMap = new TreeSet<>(map1.keySet());
