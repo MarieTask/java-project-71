669ae8c77bd92976b7ec9f8c6d6c0041e26c2f32
Мария Таскина <maryyysh2016@mail.ru>
2023-07-28 23:43:03 +0300

    fix generate

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -47,7 +47,7 @@ public class Differ {
         List<Map<String, Object>> data = BuildDifference.buildDifference(map1, map2);
         return Formatter.dataToRightFormat(data, extension);
     }
-    public static String generate(String filepath1, String filepath2) throws Exception {
-        return generate(filepath1, filepath2, "stylish");
+    public static String generate(String path1, String path2) throws Exception {
+        return generate(path1, path2, "stylish");
     }
 }
