0618d5a78b3f5a41cbf113d14924859c66c2b99d
Мария Таскина <maryyysh2016@mail.ru>
2023-08-06 19:04:34 +0300

    fix Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,12 +2,10 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.IOException;
 import java.util.Map;
 
-import static hexlet.code.Differ.getExtension;
 import static hexlet.code.ParserFactory.determineExtension;
 
 public class Parser {
